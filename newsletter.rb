f57470ada1c8e78ab8bcd62094e96a0f05e8f3d5
itsokayitsme <jcao0928@gmail.com>
2021-01-14 10:23:35 -0800

    done

--- newsletter.rb
+++ newsletter.rb
@@ -27,34 +27,47 @@ ARTICLES = [
 # Methods to generate the newsletter
 #########################
 
-def calculate_recipients
-  # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
-  # write a method that will return an array of only the subscribers who haven't unsubscribed
+def calculate_recipients 
+  new_array = []
+  SUBSCRIBERS.each do |email|
+    if !UNSUBSCRIBED.include?(email)
+      new_array << email
+    end
+  end
+  new_array
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+
+def first_n_articles(number_of_articles)
+  ARTICLES.first(number_of_articles.to_i)
 end
 
 def print_recipients
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
+ calculate_recipients.join(",")
 end
 
 def print_one_article(article)
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
+  puts article[:title]
+  puts "by: " + article[:author]
+  puts article[:text]
 end
 
 def print_many_articles(articles)
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
+  articles.each do |article|
+    print_one_article(article)
+  end
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{campus[:name]}"
 end
 
 def format_subject
@@ -71,7 +84,7 @@ def print_newsletter(number)
   print "SUBJECT: "
   format_subject
 
-  print "RECIPIENTS: "
+  print "RECIPIENTS: " + print_recipients
   print_recipients
 
   puts "\nBODY:"
@@ -81,7 +94,7 @@ def print_newsletter(number)
   puts format_footer(CAMPUS)
 
   end
-end
+
 
 def run
   # We want our program to print three articles by default,
